47a67501e5a786d77a2387f07dc17e6374db2ecd
AnaMMoreira <cinzasdanjo@hotmail.com>
2022-06-27 04:06:30 -0400

    final commit

---
+++ reaad_census_data.py
@@ -0,0 +1,7 @@
+# read Census_Data.sqlite
+
+# Python SQL toolkit and Object Relational Mapper
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
